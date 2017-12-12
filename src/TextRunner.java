3a13c8f7609600d099b0c94fb45b5496557035c0
brettwo <brettwo@BrettWo-XPS>
2017-12-11 16:36:14 -0800

    Add support for dumping computed rankings to .csv file.

--- src/TextRunner.java
+++ src/TextRunner.java
@@ -1,12 +1,15 @@
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 
 import pnfoa.evals.*;
 import pnfoa.util.CSVParser;
+import com.opencsv.*;
 
 public class TextRunner {
-	public static final String DIRECTORY = "D:\\OneDrive\\PNFOA Board\\2017 - Evaluations\\Evals App\\Move-Up";
+	public static final String DIRECTORY = "C:\\Users\\brettwo\\OneDrive\\PNFOA Board\\2017 - Evaluations\\Evals App\\Move-Up";
 	
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
@@ -49,7 +52,44 @@ public class TextRunner {
 		System.out.println("  Eval average: " + brett.getAverageScoreReceived());
 		System.out.println("  Late penalty: " + brett.getEvalPenalty());
 		System.out.println("  Unadj. COMPOSITE SCORE: " + brett.getCompositeScore());
+		
+		try {
+			CSVWriter writer = new CSVWriter(new FileWriter(DIRECTORY + "\\GeneratedRankings.csv"));
+			writer.writeNext(getCsvHeaders());
+			for (Official o : officials.values()) {
+				writer.writeNext(getCsvOutput(o));
+			}
+			
+			writer.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
     }
+	
+	
+	private static String[] getCsvOutput(Official official) {
+		List<String> values = new ArrayList<>();
+		values.add("" + official.getName());
+		values.add("" + official.getTier());
+		values.add("" + official.getAdjustedRank());
+		values.add("" + official.getAdjustedTierRank());
+		values.add("" + official.getNumGamesWorked());
+		values.add("" + official.getNumGamesWorked(Level.Varsity));
+		values.add("" + official.getAdjustedComposite());
+		values.add("" + official.getParticipationPoints());
+		values.add("" + official.getTestScore());
+		values.add("" + official.getAverageScoreReceived());
+		values.add("" + official.getEvalPenalty());
+		
+		return values.toArray(new String[0]);
+	}
+	
+	private static String[] getCsvHeaders() {
+		String[] headers = {"Name", "Tier", "Rank", "Tier Rank", "Games Worked", "Varsity Games Worked", "Composite", "Part. Points", "Test Score", "Eval. Avg.", "Penalty"}; 
+		return headers;
+	}
 	
 	private static void readPartPoints(String fileName, Map<String, Official> officials) {
 		try {
